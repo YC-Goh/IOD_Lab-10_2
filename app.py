65b245583f38401ffd4e0fca3171e3d787713fd6
Goh Yeow Chong <gohyc1993@gmail.com>
2024-04-23 20:33:27 +0800

    UPDATE: updated clean_text function

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from utils import preprocessor
 app = Flask(__name__)
 model = joblib.load('./model.joblib')
 
+##  Your basic landing page
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -14,15 +15,22 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
 
-    input = request.form['text']
+    input = request.form['userinput']
     predicted_sentiment = model.predict(pd.Series([input]))[0]
     if predicted_sentiment == 1:
         output = 'positive'
     else:
         output = 'negative'
 
-    return render_template('index.html', sentiment=f'Predicted sentiment of "{input}" is {output}.')
+    return render_template('index.html', prediction=f'Predicted sentiment of "{input}" is {output}.')
 
-
+##  name == main is executed only when python executes app.py
+##  This won't get executed when Flask is called to compile your website instead
 if __name__ == "__main__":
+    ##  Since we don't call Flask to compile the website during debug mode
+    ##  This stands in for that call
+    ##  By the way -- note that Flask + GUnicorn seems to compile webpages dynamically
+    ##  As in, only when a certain route is requested, then Flask will compile the webpage using the template and any input data
+    ##  Before passing onto GUnicorn to serve to users
+    ##  However Flask uses templates instead of html components so the server-side compilation can be much faster if the application itself is not too dynamic
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -11,10 +11,39 @@ nlp = en_core_web_sm.load()
 classifier = LinearSVC()
 
 def clean_text(text):
-    # reduce multiple spaces and newlines to only one
-    text = re.sub(r'(\s\s+|\n\n+)', r'\1', text)
-    # remove double quotes
-    text = re.sub(r'"', '', text)
+    ##  Remove all punctuation except those that may affect the actual meaning of the sentence
+    text = re.sub(r'[^a-zA-Z0-9 :;,\.!\?\'%\-]', ' ', text)
+    ##  Remove spaces between consecutive pairs of certain punctuation marks
+    text = re.sub(r'(\.) +(\.)', r'\1\2', text)
+    text = re.sub(r'([!\?]) +([!\?])', r'\1\2', text)
+    ##  Always add a space in front of commas, semicolons and colons (and remove excess when repeated)
+    text = re.sub(r'([:;,])\1*', r'\1 ', text)
+    ##  If a period is not for decimal notation, add a space in front
+    text = re.sub(r'\.(?!(?<=[0-9]\.)[0-9]|\.)', '. ', text)
+    ##  Add spaces in front of sequences of exclamation and question marks
+    text = re.sub(r'([!\?]+)', r'\1 ', text)
+    ##  For multiple periods in a row, standardise with a triple period (for the ... punctuation mark)
+    text = re.sub(r'(\.)\1{2,}', r'\1\1\1', text)
+    ##  For multiple of exclamation and question marks in a row, standardise with a double mark
+    text = re.sub(r'([!?])\1{1,}', r'\1\1', text)
+    ##  For multiple pairs of exclamation and question marks in a row, standardise with a single of each
+    text = re.sub(r'([!?])\1([!?])\2', r'\1\2', text)
+    ##  Remove all single quotes that are not immediately between 2 letters
+    text = re.sub(r'\'(?!(?<=[a-zA-Z]\')[a-zA-Z])', ' ', text)
+    ##  Remove percent signs that do not signal actual percentages
+    text = re.sub(r'(?<![0-9])%', ' ', text)
+    ##  Dashes connecting letters can arguably usually be replaced with spaces and still keep selamtic meaning, 
+    ##  but when they connect numbers they usually indicate a range and should be preserved
+    text = re.sub(r'(\d)\s*(-)\s*(\d)', r'\1\2\3', text)
+    text = re.sub(r'-(?!(?<=[0-9]-)[0-9])', ' ', text)
+    ##  Reduce spaces
+    text = re.sub(r'\s+', ' ', text)
+    text = text.strip()
+    ##  Vowels repeated more than twice are likely being done for emphasis
+    text = re.sub(r'([aeiouy])\1\1+', r'\1\1', text)
+    ##  Multiple vowels repeated twice in a row are also likely being done for emphasis
+    ##  Note that by the definition of the previous line, it is impossible for capture group 1 to be the same vowel as capture group 2
+    text = re.sub(r'([aeiouy])\1([aeiouy])\2', r'\1\2', text)
 
     return text
 	
